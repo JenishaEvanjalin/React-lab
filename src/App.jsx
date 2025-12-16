d0d266d36bba752d25675562223b6315a7e5806a
JenishaEvanjalin <jenishaevanjalinvedhanayagam@gmail.com>
2025-12-16 12:05:45 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App(){
+  return(
+    <>
+      <h1>Welcome</h1>
+      <h2>Home</h2>
+    </>
+  )
+}
+
+export default App
